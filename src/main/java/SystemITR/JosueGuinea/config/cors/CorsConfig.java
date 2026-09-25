c017640fceaee3e1f9f800ed9b3822e6aa3f2047
devjosueguinea <josueguinea@gmail.com>
2026-09-25 12:21:03 -0600

    Cambios de despliegue

--- src/main/java/SystemITR/JosueGuinea/config/cors/CorsConfig.java
+++ src/main/java/SystemITR/JosueGuinea/config/cors/CorsConfig.java
@@ -21,6 +21,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost");
         config.addAllowedOrigin("https://localhost");
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedOrigin("https://system-frontend2026.vercel.app/");
 
